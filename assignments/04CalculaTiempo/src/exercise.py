e24a9a094cf184da1771648156abf98fb4af038a
A01742269-Kyan <A01742269@itesm.mx>
2021-08-17 04:50:48 +0000

    CalculaTiempo

--- assignments/04CalculaTiempo/src/exercise.py
+++ assignments/04CalculaTiempo/src/exercise.py
@@ -2,8 +2,8 @@ def main():
     #escribe tu código abajo de esta línea
     edad= float(input("Dame tu edad: "))
     año= float(input("Dame el año actual: "))
-    actual=(año-edad)+100
-    print("Cumplirás 100 años en el año: ")
+    a=(año-edad)+100
+    print("Cumplirás 100 años en el año: ",a)
 
 
 
