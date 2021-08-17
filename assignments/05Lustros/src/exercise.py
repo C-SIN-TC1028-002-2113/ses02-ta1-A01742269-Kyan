98389281c26e3698d512d6e8707e84e7cc8b93fd
A01742269-Kyan <A01742269@itesm.mx>
2021-08-17 04:49:45 +0000

    Lustros

--- assignments/05Lustros/src/exercise.py
+++ assignments/05Lustros/src/exercise.py
@@ -1,6 +1,9 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    año de nacimiento= float(input("Dame el año de nacimiento: "))
+    año actual= float(input("Dame el año actual: "))
+    a=(año actual-año de nacimiento)/5
+    print("Los lustros que has vivido son: ",a)
 
 
 
